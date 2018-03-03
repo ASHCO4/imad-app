4c0dde3b1c393dc7261d5cf8beb4b0b2f15bcd0c
ASHCO4 <36809162+ASHCO4@users.noreply.github.com>
2018-03-03 17:05:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article1',function(req,res){
+    res.sendfile("article 1");
+});
+
+app.get('/article2',function(req,res){
+    res.sendfile("article 2");
+});
+
+app.get('/article3',function(req,res){
+    res.sendfile("article 3");
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
