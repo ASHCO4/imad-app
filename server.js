0d8bf94c01f98e4d862046aa175b44c756547ffb
ASHCO4 <36809162+ASHCO4@users.noreply.github.com>
2018-03-06 16:43:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/:articleName',function(req,res){
     res.send(createtemplate(articles[articleName]));
 });
 
-app.get('/main.js',function(req,res) {
+app.get('/ui/main.js',function(req,res) {
     res.sendFile(path.join(__dirname,'ui','main.js'));
 });
 
