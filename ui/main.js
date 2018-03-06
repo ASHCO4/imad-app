273011a967919fa3abdd301cbf3cfc43057320bc
ASHCO4 <36809162+ASHCO4@users.noreply.github.com>
2018-03-06 17:07:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,12 @@
 console.log('Loaded!');
-
+//TO CHANGE THE TEXT
 var element=document.getElementById("maintxt");
 element.innerHTML='new value';
+
+//T MOVE ELEMENT
+
+var img=document.getElementById("madi")
+img.onclick= function()
+{
+    img.style.marginLeft='100px';
+};
