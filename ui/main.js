c1b157ed111f6f42e4004a57ba6a3b39aa2d2fc3
ASHCO4 <36809162+ASHCO4@users.noreply.github.com>
2018-03-08 17:12:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,3 +16,34 @@ img.onclick= function()
 {
     var interval=setInterval(moveRight,10);
 };
+
+//counter code
+var button=document.getElementById("button");
+button.onclick()=function()
+{
+    //create request variable
+    var request=new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadyStateChange=function()
+    {
+        if(request.raedystate===XMLRequest.DONE)
+        {
+            //take some action
+            if(request.status==200)
+            {
+                var counter=request.responseText;
+                var span= Document.getElementById("count");
+                span.innerHTML= counter.toString();
+            }
+        }
+    }
+    //not yet done
+};
+
+
+//make a request
+request.open('GET','http://ashcopoci.imad.hasura-app.io/count1',true)
+request.send(null);
+    
+}
