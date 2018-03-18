af2591d53e258747945576bb5d5d0d7f6fa3c79c
ASHCO4 <36809162+ASHCO4@users.noreply.github.com>
2018-03-18 20:03:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,4 @@
-console.log('Loaded!');
-//TO CHANGE THE TEXT
-//var element=document.getElementById("maintxt");
-//element.innerHTML='new value';
 
-//T MOVE ELEMENT
-
-var img=document.getElementById("madi");
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=marginLeft + 5;
-    img.style.marginLeft=marginLeft+ 'px';
-}
-img.onclick= function()
-{
-    var interval=setInterval(moveRight,10);
-};
 
 //counter code
 var button=document.getElementById('buttn');
