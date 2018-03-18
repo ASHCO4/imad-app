f1549975d6febd14923999d34dbf3ae2aefab19a
ASHCO4 <36809162+ASHCO4@users.noreply.github.com>
2018-03-18 21:21:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,13 +35,34 @@ var name = nameInput.value;
 var submit = document.getElementById('submitbtn');
 submit.onclick = function () {
     //make request to server and send the name
+    //create request variable
+    var request=new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange=function()
+    {
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            //take some action
+            if(request.status==200)
+            {
+                //capture a list of names and render it as a list
+                var names = resquest.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i = 0; i < names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+                    }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    //not yet done
 
-    //capture a list of names and render it as a list
-    var names = ['name1', 'name2', 'name3'];
-    var list = '';
-    for (var i = 0; i < names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+
+//make a request
+request.open('GET','http://ashcopoci.imad.hasura-app.io/submit-name?name=' + name,true);
+request.send(null);
+
 };
