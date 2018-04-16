f3bc8415660650dbb2cfa9d26af2aa05477cd0d0
ASHCO4 <36809162+ASHCO4@users.noreply.github.com>
2018-04-16 11:57:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/test-db',function(req,res){
         }
         else
         {
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.row));
         }
     });
 });
